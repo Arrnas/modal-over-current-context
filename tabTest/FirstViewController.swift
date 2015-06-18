a81eae32bffaab44873834679dc8d82645e110aa
Arnas <arrnas@gmail.com>
2015-06-18 10:54:43 +0300

    cleaned up empty methods

--- tabTest/FirstViewController.swift
+++ tabTest/FirstViewController.swift
@@ -9,22 +9,10 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func showNew(sender: AnyObject) {
         let vc = storyboard?.instantiateViewControllerWithIdentifier("third") as! UIViewController
         vc.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
         presentViewController(vc, animated: true, completion: nil)
     }
-
 }
 
--- tabTest/SecondViewController.swift
+++ tabTest/SecondViewController.swift
@@ -9,22 +9,10 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func showNewVC(sender: AnyObject) {
         let vc = storyboard?.instantiateViewControllerWithIdentifier("third") as! UIViewController
         vc.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
         presentViewController(vc, animated: true, completion: nil)
     }
-
 }
 
